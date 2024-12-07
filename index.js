63001326195e3f15b2d419ed138e39a6923aa68d
Mladenka Orlić <63567763+mlaksi@users.noreply.github.com>
2024-12-07 01:44:30 +0100

    half way through adding exexercises

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require("express");
 const db = require("./db");
 const app = express();
 const cors = require("cors");
+const { ObjectId } = require("mongodb");
 require("dotenv").config();
 
 app.use(cors());
@@ -59,6 +60,54 @@ app.get("/api/users", async (req, res) => {
   } catch (err) {
     console.log(err);
     res.status(500).send("Error fetching users.");
+  }
+});
+
+// 7. You can POST to /api/users/:_id/exercises with form data description, duration, and optionally date. If no date is supplied, the current date will be used.
+// 8. The response returned from POST /api/users/:_id/exercises will be the user object with the exercise fields added.
+app.post("/api/users/:_id/exercises", async (req, res) => {
+  try {
+    const database = db.getDb();
+
+    //pick up user data from form and route params
+    const formattedDate = new Date(req.body.date).toDateString();
+    const duration = req.body.duration;
+    const description = req.body.description;
+    const id = new ObjectId(req.params._id);
+
+    //find user with corresponding id
+    const user = await database.collection("users").findOne({ _id: id });
+
+    //if there is no user, respond with error
+    if (!user) {
+      res.status(404).send("User not found");
+    }
+
+    res.json({
+      _id: user._id,
+      username: user.username,
+      date: formattedDate,
+      duration: duration,
+      description: description,
+    });
+
+    // await database.collection("exercises").insertOne({
+    //   _id: user._id,
+    //   username: user.username,
+    //   date: formattedDate,
+    //   duration: duration,
+    //   description: description,
+    // });
+    // const exercise = database
+    //   .collection("exercises")
+    //   .findOne({ _id: user._id });
+    // if (exercise) {
+    //   res.json({ exercise });
+    // }
+    //await database.collection("exercises").insertOne({});
+  } catch (err) {
+    console.log(err);
+    res.status(500).send("Error inserting exercise.");
   }
 });
 
